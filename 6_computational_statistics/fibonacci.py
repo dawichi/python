6e6199caed35f51310f689c812b845e7f1994dac
David M. Fajardo <davidmf99@gmail.com>
2021-09-11 12:31:33 +0200

    fibonacci

---
+++ 6_computational_statistics/fibonacci.py
@@ -0,0 +1,24 @@
+# If u try to calculate a number bigger than 30-35, will start to take so much time
+# because the function must calculate every small number multiple times in a exponential growth
+def fibonacci_recursive(n):
+	if n == 0 or n == 1:
+		return 1
+	return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+# Best solution: memorize the result in a dictionary and now is instant! Even if u calc f(500)
+def fibonacci_dynamic(n, memo = {}):
+	if n == 0 or n == 1:
+		return 1
+
+	try:
+		return memo[n]
+	except KeyError:
+		result = fibonacci_dynamic(n - 1, memo) + fibonacci_dynamic(n - 2, memo)
+		memo[n] = result
+		return result
+
+
+if __name__ == '__main__':
+	n = int(input('Type a number: '))
+	print(fibonacci_dynamic(n))
